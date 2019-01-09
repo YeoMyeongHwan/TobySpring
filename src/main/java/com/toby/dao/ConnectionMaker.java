a04c6fbf08a63a93499f572132ce3c4b2f66e465
yeomyeonghwan <yeomyeonghwan@gmail.com>
2019-01-09 23:12:55 +0900

    1.5 스프링의IoC

---
+++ src/main/java/com/toby/dao/ConnectionMaker.java
@@ -0,0 +1,8 @@
+package com.toby.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionMaker {
+	public Connection makeConnection() throws ClassNotFoundException, SQLException;
+}
