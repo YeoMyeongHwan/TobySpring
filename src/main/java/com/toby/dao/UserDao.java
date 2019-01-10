60df6e9a18e7d8a1cf9de2dffd46a47bd91ec98d
yeomyeonghwan <yeomyeonghwan@gmail.com>
2019-01-10 22:12:53 +0900

    1.8 XML을 이용할 설정

--- src/main/java/com/toby/dao/UserDao.java
+++ src/main/java/com/toby/dao/UserDao.java
@@ -5,16 +5,21 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.toby.vo.User;
 
 public class UserDao {
-	private ConnectionMaker connectionMaker;
+	private DataSource dataSource;
 	
-	public UserDao(ConnectionMaker connectionMaker) {
-		this.connectionMaker = connectionMaker;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
 	}
-	public void add(User user) throws ClassNotFoundException, SQLException{
-		Connection c = connectionMaker.makeConnection();
+	public void add(User user) throws SQLException{
+		Connection c = dataSource.getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
 		ps.setString(1,  user.getId());
@@ -25,14 +30,15 @@ public class UserDao {
 		ps.close();
 		c.close();
 	}
-	public User get(String id) throws ClassNotFoundException, SQLException{
-		Connection c = connectionMaker.makeConnection();
+	public User get(String id) throws SQLException{
+		Connection c = dataSource.getConnection();
 		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1,  id);
 		
 		ResultSet rs = ps.executeQuery();
 		rs.next();
+		
 		User user = new User();
 		user.setId(rs.getString("id"));
 		user.setName(rs.getString("name"));
